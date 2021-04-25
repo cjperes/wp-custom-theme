d6753141ab1e8992a2611e6e4504802b4ea23045
Caio Peres <caio@nork.digital>
2021-04-25 08:31:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
-<?php get_header(); ?>
+<?php get_header(); //pega o header do wp 
+?>
+
+
 
 <?php if (have_posts()) {
   while (have_posts()) {
     the_post();
   } { ?>
 
-    <h1><?php the_title(); ?></h1>
-    <main><?php the_content(); ?></main>
+    <h1><?php the_title(); //exibe o titulo
+        ?></h1>
+    <main><?php the_content(); //exibe o conteudo
+          ?></main>
 
 <?php }
 } ?>
 
 
 
-<?php get_footer(); ?>
+<?php get_footer(); //pega o footer do wp 
+?>
