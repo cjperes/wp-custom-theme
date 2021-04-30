0f55d866a656f545777d4997f6810119a725a8e9
Caio Peres <caio@nork.digital>
2021-04-30 15:02:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <h1><?php the_title(); //exibe o titulo
         ?></h1>
-    <main><?php the_content(); //pucha todos os conteúdos
+    <main><?php the_content(); //puxa todos os conteúdos
           ?></main>
 
 <?php }
