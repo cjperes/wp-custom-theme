419f4254d5d0c1e24b48fdcd9193c6786acd3d18
Caio Peres <caio@nork.digital>
2021-07-11 20:37:49 -0300

    header

--- header.php
+++ header.php
@@ -5,21 +5,42 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?php bloginfo('name') ?> | <?php wp_title(); ?></title>
+  <title><?php bloginfo('name') ?> <?php wp_title('|'); ?></title>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
   <?php
-  $img_url = get_stylesheet_directory_uri() . '/img/'
+  $img_url = get_stylesheet_directory_uri() . '/img';
+  $cart_count = WC()->cart->get_cart_contents_count();
   ?>
 
-  <header class="header">
-
-    <a href="#" target="_blank" rel="noopener noreferrer"> <img src="<?= $img_url ?>/handel.svg" alt=""> </a>
+  <header class="header container">
+    <a href="/"><img src="<?= $img_url; ?>/handel.svg" alt="Handel" /></a>
     <div class="busca">
-      <?php get_product_search_form(); ?>
+      <form action="<?php bloginfo('url'); ?>/loja/" method="get">
+        <input type="text" name="s" id="s" placeholder="Buscar" value="<?php the_search_query(); ?>">
+        <input type="text" name="post_type" value="product" class="hidden">
+        <input type="submit" id="searchbutton" value="Buscar">
+      </form>
     </div>
-
+    <nav class="conta">
+      <a href="/minha-conta" class="minha-conta">Minha Conta</a>
+      <a href="/carrinho" class="carrinho">Carrinho
+        <?php if ($cart_count) { ?>
+          <!-- contador de produtos carrinho -->
+          <span class="carrinho-count"><?= $cart_count; ?></span> <!-- contador de produtos carrinho -->
+        <?php } ?>
+      </a>
+    </nav>
   </header>
+
+  <div class="menu-categorias">
+    <?php
+    wp_nav_menu([
+      'menu' => 'categorias',
+      'container' => 'nav',
+      'container_class' => 'menu-categorias'
+    ]);
+    ?></div>
