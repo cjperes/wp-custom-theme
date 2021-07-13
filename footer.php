c94664791ba1cde57ecb6c30fd19be1c4ae8b4db
Caio Peres <caio@nork.digital>
2021-07-13 19:47:17 -0300

    vai

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-
 <?php wp_footer(); ?>
+<script src="<?php get_stylesheet_directory_uri(); ?> /js/slide.js "></script>
+<script src="<?php get_stylesheet_directory_uri(); ?> /js/script.js "></script>
 </body>
+
 </html>
