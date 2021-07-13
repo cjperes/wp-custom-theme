eaf26027b0f599b65377e9f283e1710794e10223
Caio Peres <caio@nork.digital>
2021-07-13 20:38:14 -0300

    array e loop slide

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 <?php wp_footer(); ?>
-<script src="<?php get_stylesheet_directory_uri(); ?> /js/slide.js "></script>
-<script src="<?php get_stylesheet_directory_uri(); ?> /js/script.js "></script>
-</body>
+<script async type="text/javascript" src="<?php echo esc_url(get_stylesheet_directory_uri() . '/js/slide.js'); ?>">
+</script>
 
-</html>
+<script async type="text/javascript" src="<?php echo esc_url(get_stylesheet_directory_uri() . '/js/script.js'); ?>">
+</script>
+
+< /body>
+
+  < /html>
--- page-home.php
+++ page-home.php
@@ -2,14 +2,61 @@
 // Template name: Home
 get_header(); ?>
 
-<?php if(have_posts()) { while (have_posts()) { the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <main><?php the_content(); ?></main>
-  <pre>
-  <?php
 
-  ?>
-  </pre>
-<?php } } ?>
+<?php
+//array de produtos por tag
+$products_slide = wc_get_products([
+  'limit' => 6,
+  'tag' => ['slide'],
+]);
+
+
+
+//formata a saida dos produtos limpando informações desnecessárias
+function format_products($products)
+{
+  $products_final = [];
+  foreach ($products as $product) {
+    $products_final[] = [
+      'name' => $product->get_name(),
+      'preco' => $product->get_price_html(),
+      'link' => $product->get_permalink(),
+      //retorna o array da img: url, width e height
+      'img' => wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
+    ];
+  }
+  return $products_final;
+}
+$slide = format_products($products_slide);
+
+?>
+
+<?php if (have_posts()) {
+  while (have_posts()) {
+    the_post(); ?>
+
+
+    <section class="slide-wrapper">
+      <ul class="slide">
+        <li class="slide-item">
+          <img src="<?= $product['img']; ?>" alt="<? $product['name']; ?>">
+
+          <div class="slide-info">
+
+
+          </div>
+        </li>
+        <?php foreach ($slide as $product) { ?>
+          <li><?= $product['name']; ?></li>
+        <?   } ?>
+      </ul>
+
+
+
+    </section>
+
+
+<?php }
+} ?>
 
 <?php get_footer(); ?>
